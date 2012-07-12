833a45daa8d1dee462563a981de0bec3f27869f1
Nicholas H.Tollervey <ntoll@ntoll.org>
2012-07-12 21:59:04 +0100

    Draws a board

--- board.py
+++ board.py
@@ -1,5 +1,27 @@
+import string
+
 def draw(board):
-    pass
+    line = []
+    for i in range(9):
+        value = board[i]
+        if not value:
+            val_to_display = ' '
+        elif isinstance(value, str):
+            val_to_display = ' %s ' % value
+        else:
+            vals = []
+            for v in value:
+                token = 'X'
+                if v % 2:
+                    token = 'O'
+                vals.append('%s%d' % (token, v))
+            val_to_display = ', '.join(vals)
+        line.append(string.center(val_to_display, 16))
+        if i in [2, 5]:
+            line.append('\n-----------------------------------------------------------\n')
+        elif i < 8:
+            line.append(' || ')
+    print ''.join(line)
 
 def init():
     return []
