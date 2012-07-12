9a38c8103d5b9241f4ec943f14405c694a790fbf
Florian Rathgeber <florian.rathgeber@gmail.com>
2012-07-12 21:54:02 +0100

    Resolve cycle

--- game.py
+++ game.py
@@ -3,7 +3,10 @@ import copy
 import board
 import state
 
-def turn(board):
+def playernum(r):
+    return (r % 2) + 1
+def turn(r, board):
+    print "Player %d's turn:" % playernum(r)
     p1 = raw_input('Enter first move [0-8] ')
     p2 = raw_input('Enter second move [0-8] ')
     newboard = copy.deepcopy(board)
@@ -15,8 +18,15 @@ def turn(board):
         return turn(r, board)
     return newboard
 
-def collapse(board, cycle):
-    return board
+def prompt_resolve(r, cycle):
+    print "Player %d, collapse the following cycle:" % playernum(r+1), cycle
+    pos = raw_input("Enter a position ")
+    sym = raw_input("Enter a symbol (X/O) ")
+    sym_parity = {'X':0 , 'O':1}[sym]
+    if not sym_parity in [i % 2 for i in  board[pos]]:
+        print 'Illegal choice'
+        return prompt_resolve(r, cycle)
+    return pos, sym
 
 def play():
     b = board.init()
@@ -25,8 +35,8 @@ def play():
         board.draw(b)
         c = state.find_cycle(b)
         if c:
-            square, symbol = prompt_resolve()
-            b = state.collapse(b, square, symbol)
+            pos, symbol = prompt_resolve(r, c)
+            b = state.collapse(b, pos, symbol)
         if state.won(b):
-            print "Player %d won!" % ((round % 2) + 1)
+            print "Player %d won!" % playernum(r)
             break
