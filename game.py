db4a20ef29145c72cabc9e827457f761bd0544dc
Florian Rathgeber <florian.rathgeber@gmail.com>
2012-07-12 21:37:03 +0100

    Add turn method

--- game.py
+++ game.py
@@ -1,16 +1,27 @@
+import copy
+
 import board
 import state
 
 def turn(board):
-    return board
+    p1 = raw_input('Enter first move [0-8] ')
+    p2 = raw_input('Enter second move [0-8] ')
+    newboard = copy.deepcopy(board)
+    try:
+        newboard[p1].append(r)
+        newboard[p2].append(r)
+    except:
+        print "Field has already collapsed"
+        return turn(r, board)
+    return newboard
 
 def collapse(board, cycle):
     return board
 
 def play():
     b = board.init()
-    for round in range(1,9):
-        b = turn(b)
+    for r in range(1,9):
+        b = turn(r, b)
         board.draw(b)
         c = state.find_cycle(b)
         if c:
